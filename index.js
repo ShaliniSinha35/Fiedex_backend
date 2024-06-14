7b4b2f786a585024476add2aea0a529ab3296fa2
ShaliniSinha35 <shalinisinha3560@gmail.com>
2024-06-14 18:21:05 +0530

    14.06.24

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const mysql = require("mysql");
 const app = express();
-const port = process.env.port || 6000;
+const port = process.env.port || 3001;
 const bodyParser = require("body-parser");
 const cors= require('cors');
 
 app.use(cors())
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
@@ -26,9 +25,15 @@ const connection = mysql.createPool({
   
   
   host: "fiedex.com",
-  user: "u743095106_fiedex_user",
-  password: "osS?brJ=tO7",
-  database:"u743095106_fiedex_db"
+  user: "fiedex_web_user",
+  password: "X2p^1I)f7!ju",
+  database:"fiedex_web_db"
+
+
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database:"fiedex"
 });
 
 
@@ -54,43 +59,43 @@ handleDisconnect();
 
 
 
-app.post("/register", (req, res) => {
+app.post("/fiedex/register", (req, res) => {
   const { name, mobile_number, country_code, email, password, refer_code } = req.body;
 
   // Check if email already exists
-  const emailCheckQuery = `SELECT COUNT(id) AS count FROM register WHERE email = ?`;
+  const emailCheckQuery = `SELECT COUNT(*) AS count FROM register WHERE email = ?`;
   connection.query(emailCheckQuery, [email], (err, result) => {
-      if (err) {
-          console.error('Error checking email existence: ' + err.stack);
-          res.status(500).send('Error checking email existence');
-          return;
-      }
-      
-      if (result[0].count > 0) {
-          // Email already exists, return error response
-          res.status(400).send('Email already exists');
-      } else {
-          // Insert into register table
-          const insertQuery = `INSERT INTO register (name, mobile_number, country_code, email, password, refer_code, status, entry_date) VALUES (?, ?, ?, ?, ?, ?, 'Active', NOW())`;
-          const values = [name, mobile_number, country_code, email, password, refer_code];
+    if (err) {
+      console.error('Error checking email existence: ' + err.stack);
+      res.status(500).send('Error checking email existence');
+      return;
+    }
 
-          connection.query(insertQuery, values, (insertErr, insertResult) => {
-              if (insertErr) {
-                  console.error('Error inserting into register table: ' + insertErr.stack);
-                  res.status(500).send('Error inserting into register table');
-                  return;
-              }
-              console.log('Inserted into register table with ID: ' + insertResult.insertId);
-              res.status(200).send('Inserted into register table');
-          });
-      }
+    console.log(result[0].count)
+    if (result[0].count > 0) {
+      // Email already exists, return error response
+      res.status(400).send('Email already exists');
+      
+    } else {
+      // Insert into register table
+      const insertQuery = `INSERT INTO register (name, mobile_number, country_code, email, password, refer_code, status, entry_date) VALUES (?, ?, ?, ?, ?, ?, '1', NOW())`;
+      const values = [name, mobile_number, country_code, email, password, refer_code];
+
+      connection.query(insertQuery, values, (insertErr, insertResult) => {
+        if (insertErr) {
+          console.error('Error inserting into register table: ' + insertErr.stack);
+          res.status(500).send('Error inserting into register table');
+          return;
+        }
+        console.log('Inserted into register table with ID: ' + insertResult.insertId);
+        res.status(200).send('Inserted into register table');
+      });
+    }
   });
 });
 
 
-
-
-app.get("/verify", (req, res) => {
+app.get("/fiedex/verify", (req, res) => {
     const { email, password } = req.query; // Use req.query instead of req.body
 
     console.log(email, password);
@@ -113,11 +118,7 @@ app.get("/verify", (req, res) => {
     });
 });
 
-  
-
-
-
-app.get("/quizQuestions",(req,res)=>{
+app.get("/fiedex/quizQuestions",(req,res)=>{
   // console.log("quizQuestions api")
   connection.query(
     "SELECT * FROM `question` WHERE STATUS ='Active'",
@@ -133,8 +134,7 @@ app.get("/quizQuestions",(req,res)=>{
   );
 })
 
-
-app.get("/about",(req,res)=>{
+app.get("/fiedex/about",(req,res)=>{
   connection.query(
     "SELECT `page_menu`,`content` FROM `static_page` WHERE id = 12",
     (error, results) => {
@@ -149,7 +149,7 @@ app.get("/about",(req,res)=>{
   );
 })
 
-app.get("/privacy",(req,res)=>{
+app.get("/fiedex/privacy",(req,res)=>{
   connection.query(
     "SELECT `page_menu`,`content` FROM `static_page` WHERE id = 13",
     (error, results) => {
@@ -163,7 +163,7 @@ app.get("/privacy",(req,res)=>{
     }
   );
 })
-app.get("/terms",(req,res)=>{
+app.get("/fiedex/terms",(req,res)=>{
   connection.query(
     "SELECT `page_menu`,`content` FROM `static_page` WHERE id = 14",
     (error, results) => {
@@ -178,7 +178,7 @@ app.get("/terms",(req,res)=>{
   );
 })
 
-app.get("/socials",(req,res)=>{
+app.get("/fiedex/socials",(req,res)=>{
   
   connection.query(
     "SELECT `whatsapp`, `email`, `mobile_1` `cus_care_num` FROM `website_data`",
@@ -192,12 +192,7 @@ app.get("/socials",(req,res)=>{
   ); 
 })
 
-
-
-
-
-
-app.get("/walletDetails",(req,res)=>{
+app.get("/fiedex/walletDetails",(req,res)=>{
   
   const {userId}= req.query
     
@@ -217,7 +212,7 @@ app.get("/walletDetails",(req,res)=>{
     });
   })
 
-app.get("/wallet",(req,res)=>{
+app.get("/fiedex/wallet",(req,res)=>{
   
 const {userId}= req.query
   
@@ -234,21 +229,20 @@ const {userId}= req.query
   });
 })
 
-app.get("/updateWallet", (req, res) => {
+app.get("/fiedex/updateWallet", (req, res) => {
   console.log("update wallet is called");
   const { userId, amount } = req.query;
 
-  // Parse amount to integer
   const newAmountToAdd = parseInt(amount);
 
   // Check if userId and amount are valid
-  if (!userId || !newAmountToAdd || isNaN(newAmountToAdd)) {
+  if (!userId || isNaN(newAmountToAdd)) {
     console.error("Invalid userId or amount");
     res.status(400).send("Invalid userId or amount");
     return;
   }
 
-  // First, try to retrieve the current amount from the database
+  // Retrieve the current amount from the database
   const selectSql = `SELECT amount FROM wallet_log WHERE user_id = ?`;
   connection.query(selectSql, [userId], (err, results) => {
     if (err) {
@@ -257,66 +251,48 @@ app.get("/updateWallet", (req, res) => {
       return;
     }
 
-    // Check if the user exists in the database
     const userExists = results.length > 0;
 
-    // If the user doesn't exist, insert a new record
     if (!userExists) {
       const insertSql = `INSERT INTO wallet_log (user_id, amount, cdate, cby, status) VALUES (?, ?, NOW(), ?, '1')`;
-      connection.query(
-        insertSql,
-        [userId, newAmountToAdd, userId],
-        (insertErr, insertResult) => {
-          if (insertErr) {
-            console.error("Error inserting wallet data:", insertErr);
-            res.status(500).send("Error inserting wallet data");
-            return;
-          }
-
-          console.log("Insert successful for new user");
-          res.json(insertResult);
+      connection.query(insertSql, [userId, newAmountToAdd, userId], (insertErr, insertResult) => {
+        if (insertErr) {
+          console.error("Error inserting wallet data:", insertErr);
+          res.status(500).send("Error inserting wallet data");
+          return;
         }
-      );
+
+        console.log("Insert successful for new user");
+        res.json(insertResult);
+      });
     } else {
-      // User exists, update the existing record
       const currentAmount = parseInt(results[0].amount);
 
-      // Check if the current amount is a valid number
       if (isNaN(currentAmount)) {
         console.error("Invalid current amount value in database");
         res.status(500).send("Invalid current amount value in database");
         return;
       }
 
-      // Calculate the new total amount
       const newTotalAmount = currentAmount + newAmountToAdd;
-
-      // Update the amount in the database
       const updateSql = `UPDATE wallet_log SET amount = ? WHERE user_id = ?`;
-      connection.query(
-        updateSql,
-        [newTotalAmount, userId],
-        (updateErr, updateResult) => {
-          if (updateErr) {
-            console.error("Error updating user data:", updateErr);
-            res.status(500).send("Error updating user data");
-            return;
-          }
-
-          console.log("Wallet amount updated successfully");
-          res.json(updateResult);
+      connection.query(updateSql, [newTotalAmount, userId], (updateErr, updateResult) => {
+        if (updateErr) {
+          console.error("Error updating user data:", updateErr);
+          res.status(500).send("Error updating user data");
+          return;
         }
-      );
+
+        console.log("Wallet amount updated successfully");
+        res.json(updateResult);
+      });
     }
   });
 });
 
-
-
-app.get("/",(req,res)=>{
+app.get("/fiedex/",(req,res)=>{
      res.send("hello from fiedex")
 })
-
 
 app.listen(port, () => {
   console.log("server is running", {port});
